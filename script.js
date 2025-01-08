af37b882a3ef59a389fa4c494192eba9db8359d1
TheCyberAyo <ndamaseayongezwa@gmail.com>
2025-01-08 15:39:46 +0200

    added all ball items

--- script.js
+++ script.js
@@ -12,20 +12,51 @@ let secondCard = false;
 
 //item array
 const items = [
-    {name: "tennisball", image: "bee.png"},
-    {name: "soccer", image: "bee.png"},
-    {name: "American", image: "bee.png"},
-    {name: "football", image: "bee.png"},
-    {name: "cricket", image: "bee.png"},
-    {name: "rugby", image: "bee.png"},
-    {name: "volleyball", image: "bee.png"},
-    {name: "golfball", image: "bee.png"},
-    {name: "marble", image: "bee.png"},
-    {name: "netball", image: "bee.png"},
-    {name: "squashball", image: "bee.png"},
-    {name: "Basketball", image: "bee.png"},
-    {name: "Softball", image: "bee.png"},
-    {name: "pingpong", image: "bee.png"},
-    {name: "", image: "bee.png"}
-
+    {name: "tennisball", image: "tennis.png"},
+    {name: "american F", image: "American.png"},
+    {name: "football", image: "foot ball.png"},
+    {name: "cricket", image: "cricket.png"},
+    {name: "rugby", image: "rugby.png"},
+    {name: "volleyball", image: "volley ball.png"},
+    {name: "golfball", image: "golf ball.png"},
+    {name: "marble", image: "marble.png"},
+    {name: "netball", image: "netball.png"},
+    {name: "squashball", image: "squashball.png"},
+    {name: "basketball", image: "basket ball.png"},
+    {name: "softball", image: "softball.png"},
+    {name: "pingpong", image: "pingpong ball.png"},
+    {name: "q-ball", image: "q ball.png"},
+    {name: "number 8", image: "number 8.png"},
+    {name: "gold Marble", image: "gold marble"},
+    {name: "moon", image: "moon.png"},
+    {name: "earth", image: "earth.png"},
+    {name: "gym ball", image: "gym ball.png"},
+    {name: "balloon", image: "balloon.png"},
+    {name: "dragon ball", image: "dragon ball.png"},
+    {name: "time stone", image: "time stone.png"},
+    {name: "dumb bell", image: "dumb bell.png"},
+    {name: "bowling ball", image: "bowling-ball"},
+    {name: "meat ball", image: "meat ball.png"},
+    {name: "light bulb", image: "light bulb.png"},
+    {name: "rocky ball", image: "rocky ball.png"},
+    {name: "sandy ball", image: "sandy ball.png"},
+    {name: "fire ball", image: "fire ball.png"},
+    {name: "silver ball", image: "silver ball.png"},
+    {name: "black ball", image: "black ball.png"},
+    {name: "grass ball", image: "grass ball"},
 ]
+
+// initial time
+let seconds = 0,
+  minutes = 0;
+
+//initials moves and win count
+let movesCount = 0,
+  winCount = 0;
+
+//For Timer
+const timeGenerator = () => {
+    seconds =+ 1;
+    //minutes logic
+    if(seconds <= 60)
+}
